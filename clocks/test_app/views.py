06e4638562adad6ee977e617493e947caa4073bd
phonik <askar.a.shakirov@gmail.com>
2024-08-21 20:07:20 +0300

    Допилил форму для отправки url

--- clocks/test_app/views.py
+++ clocks/test_app/views.py
@@ -1,28 +1,18 @@
-# from django.shortcuts import render
-#
-#
-# def shorten_url(request):
-#     if request.method == 'GET':
-#         if form := request.GET["url"] is not None:
-#             return render(request, 'index.html', {'form': form})
-#         return render(request, 'index.html')
-#     else:
-#         form = "Пусто"
-#
-
-
 from django.shortcuts import render
 from .forms import URLShortenerForm
 
 
 def shorten_url(request):
-    if request.method == 'POST':
-        form = URLShortenerForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
+    if request.method == "POST":
+        # form = URLShortenerForm(request.POST)
+        # if form.is_valid():
+        #     print(form.cleaned_data)
+        form = URLShortenerForm()
+        url = request.POST.get("url")
+        print(url)
+
     else:
         form = URLShortenerForm()
 
-    return render(request, 'index.html', {
-        'form': form,
-    })
+    return render(request, "index.html",
+                  {"form": form, "existing_urls": ["example.com"]})
