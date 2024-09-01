32ca119cb16555786771da30dfbdf45ba2d81a9a
phonik <askar.a.shakirov@gmail.com>
2024-09-01 11:25:31 +0300

    Сделал отправку данных всем участникам группы

--- clocks/settings/settings.py
+++ clocks/settings/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-4uesufsmtlw(@ffq4ro%!he-2u=0-gpf$vfbg=tof+6_+4cr@*
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,7 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "settings.wsgi.application"
 ASGI_APPLICATION = "settings.asgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -105,6 +102,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
@@ -116,7 +121,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
--- clocks/ws/consumers.py
+++ clocks/ws/consumers.py
@@ -1,65 +1,87 @@
 import json
 
-from asgiref.sync import sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404
 from meetings.models import Meeting
 from meetings.serializers import MeetingSerializer
 from rooms.models import Room
 
 
-class RoomConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
+class RoomConsumer(WebsocketConsumer):
 
-    async def disconnect(self, close_code):
-        await self.close()
+    def connect(self):
+        async_to_sync(self.channel_layer.group_add)("room", self.channel_name)
+        self.accept()
 
-    async def receive(self, text_data):
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)("room", self.channel_name)
+
+    def receive(self, text_data):
         try:
             text_data_json = json.loads(text_data)
             action = text_data_json.get("action")
 
             if action == "refresh_participants":
-                await self.send_response(await self.get_data())
+                response = self.get_data()
+                async_to_sync(self.channel_layer.group_send)(
+                    "room",
+                    {
+                        "type": "chat.message",
+                        "message": json.dumps(response),
+                    }
+                )
 
             elif action == "submit_vote":
                 user_id = text_data_json.get("user_id")
                 vote = text_data_json.get("vote")
-                await self.save_vote(user_id, vote)
+                response = self.save_vote(user_id, vote)
+
+                async_to_sync(self.channel_layer.group_send)(
+                    "room",
+                    {
+                        "type": "chat.message",
+                        "message": json.dumps(response),
+                    }
+                )
             else:
-                await self.send_response({"message": "unknown action"})
+                async_to_sync(self.channel_layer.group_send)(
+                    "room",
+                    {
+                        "type": "chat.message",
+                        "message": json.dumps({"message": "unknown action"}),
+                    }
+                )
         except json.JSONDecodeError:
-            await self.send_response({"message": "Invalid JSON format"})
+            async_to_sync(self.channel_layer.group_send)(
+                "room",
+                {
+                    "type": "chat.message",
+                    "message": json.dumps({"message": "Invalid JSON format"}),
+                }
+            )
 
-    async def send_response(self, data):
-        await self.send(text_data=json.dumps(data))
+    def chat_message(self, event):
+        self.send(text_data=event["message"])
 
-    @sync_to_async
     def get_meeting(self):
         return get_object_or_404(Meeting, id=1)
 
-    @sync_to_async
     def get_room(self, meeting_id):
         return get_object_or_404(Room, current_meeting=meeting_id)
 
-    async def get_data(self):
-        meeting = await self.get_meeting()
+    def get_data(self):
+        meeting = self.get_meeting()
         serializer = MeetingSerializer(meeting, fields=["votes"])
         return serializer.data
 
-    @sync_to_async
-    def save_vote_sync(self, user_name, vote):
-        meeting: Meeting = Meeting.objects.get(id=1)
-        meeting.votes[user_name] = vote
-        meeting.save()
-
-    async def save_vote(self, user_name, vote):
-        meeting: Meeting = await self.get_meeting()
-        room = await self.get_room(meeting.id)
+    def save_vote(self, user_name, vote):
+        meeting = self.get_meeting()
+        room = self.get_room(meeting.id)
 
         if not any(user_name in d for d in room.users):
-            await self.send_response({"error": "Participant doesn't exist"})
+            return {"error": "Participant doesn't exist"}
         else:
-            await self.save_vote_sync(user_name, vote)
-            await self.send_response({"success": "Vote saved successfully"})
+            meeting.votes[user_name] = vote
+            meeting.save()
+            return self.get_data()
