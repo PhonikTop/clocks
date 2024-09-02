53072566035b851ab0b78b9d3933dfe4c9625d4b
phonik <askar.a.shakirov@gmail.com>
2024-09-02 11:24:28 +0300

    Сделал consumers.py асинхронным

--- clocks/ws/consumers.py
+++ clocks/ws/consumers.py
@@ -1,56 +1,74 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.shortcuts import get_object_or_404
 from meetings.models import Meeting
 from meetings.serializers import MeetingSerializer
 from rooms.models import Room
 
 
-class RoomConsumer(WebsocketConsumer):
-    def connect(self):
+class RoomConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.group_name = f"room_{self.room_id}"
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
-        self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.group_name, self.channel_name)
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
         try:
             data = json.loads(text_data)
             action = data.get("action")
             response = {"message": "unknown action"}
 
             if action == "refresh_participants":
-                response = self.get_data()
+                response = await self.get_data()
             elif action == "submit_vote":
-                response = self.save_vote(data.get("user_id"), data.get("vote"))
+                user_id = data.get("user_id")
+                vote = data.get("vote")
+                response = await self.save_vote(user_id, vote)
 
-            self.send_group_message(response)
+            await self.send_group_message(response)
         except json.JSONDecodeError:
-            self.send_group_message({"message": "Invalid JSON format"})
+            await self.send_group_message({"message": "Invalid JSON format"})
 
-    def send_group_message(self, message):
-        async_to_sync(self.channel_layer.group_send)(
+    async def send_group_message(self, message):
+        await self.channel_layer.group_send(
             self.group_name,
-            {"type": "chat.message", "message": json.dumps(message)},
+            {
+                "type": "chat.message",
+                "message": json.dumps(message),
+            }
         )
 
-    def get_meeting(self):
+    @sync_to_async
+    def get_meeting_sync(self):
         return get_object_or_404(Meeting, room_id=self.room_id)
 
-    def get_data(self):
-        meeting = self.get_meeting()
+    async def get_meeting(self):
+        return await self.get_meeting_sync()
+
+    async def get_data(self):
+        meeting = await self.get_meeting()
         serializer = MeetingSerializer(meeting, fields=["votes"])
         data = serializer.data
         data["votes"] = {k: v for k, v in data.get("votes", {}).items() if v is not None}
         return data
 
-    def save_vote(self, user_name, vote):
-        meeting = self.get_meeting()
+    @sync_to_async
+    def save_vote_sync(self, user_name, vote):
+        meeting = get_object_or_404(Meeting, room_id=self.room_id)
         room = get_object_or_404(Room, id=self.room_id)
 
         if not any(user_name in d for d in room.users):
@@ -58,4 +76,16 @@ class RoomConsumer(WebsocketConsumer):
 
         meeting.votes[user_name] = vote
         meeting.save()
-        return self.get_data()
+
+        serializer = MeetingSerializer(meeting, fields=["votes"])
+        data = serializer.data
+        data["votes"] = {k: v for k, v in data.get("votes", {}).items() if v is not None}
+        return data
+
+    async def save_vote(self, user_name, vote):
+        return await self.save_vote_sync(user_name, vote)
+
+    async def chat_message(self, event):
+        message = event["message"]
+
+        await self.send(text_data=message)
