35f8d360c4c8184fcaf93abe3a50d8940c0d421e
phonik <askar.a.shakirov@gmail.com>
2024-09-02 11:18:43 +0300

    Почистил код

--- clocks/ws/consumers.py
+++ clocks/ws/consumers.py
@@ -9,85 +9,53 @@ from rooms.models import Room
 
 
 class RoomConsumer(WebsocketConsumer):
-
     def connect(self):
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
-        async_to_sync(self.channel_layer.group_add)(f"room_{self.room_id}", self.channel_name)
+        self.group_name = f"room_{self.room_id}"
+        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
         self.accept()
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(f"room_{self.room_id}", self.channel_name)
+        async_to_sync(self.channel_layer.group_discard)(self.group_name, self.channel_name)
 
     def receive(self, text_data):
         try:
-            text_data_json = json.loads(text_data)
-            action = text_data_json.get("action")
+            data = json.loads(text_data)
+            action = data.get("action")
+            response = {"message": "unknown action"}
 
             if action == "refresh_participants":
                 response = self.get_data()
-                async_to_sync(self.channel_layer.group_send)(
-                    f"room_{self.room_id}",
-                    {
-                        "type": "chat.message",
-                        "message": json.dumps(response),
-                    }
-                )
-
             elif action == "submit_vote":
-                user_id = text_data_json.get("user_id")
-                vote = text_data_json.get("vote")
-                response = self.save_vote(user_id, vote)
+                response = self.save_vote(data.get("user_id"), data.get("vote"))
 
-                async_to_sync(self.channel_layer.group_send)(
-                    f"room_{self.room_id}",
-                    {
-                        "type": "chat.message",
-                        "message": json.dumps(response),
-                    }
-                )
-            else:
-                async_to_sync(self.channel_layer.group_send)(
-                    f"room_{self.room_id}",
-                    {
-                        "type": "chat.message",
-                        "message": json.dumps({"message": "unknown action"}),
-                    }
-                )
+            self.send_group_message(response)
         except json.JSONDecodeError:
-            async_to_sync(self.channel_layer.group_send)(
-                f"room_{self.room_id}",
-                {
-                    "type": "chat.message",
-                    "message": json.dumps({"message": "Invalid JSON format"}),
-                }
-            )
+            self.send_group_message({"message": "Invalid JSON format"})
 
-    def chat_message(self, event):
-        self.send(text_data=event["message"])
+    def send_group_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {"type": "chat.message", "message": json.dumps(message)},
+        )
 
     def get_meeting(self):
         return get_object_or_404(Meeting, room_id=self.room_id)
 
-    def get_room(self):
-        return get_object_or_404(Room, id=self.room_id)
-
     def get_data(self):
         meeting = self.get_meeting()
         serializer = MeetingSerializer(meeting, fields=["votes"])
-        data = serializer.data.copy()
-
-        if "votes" in data:
-            data["votes"] = {k: v for k, v in data["votes"].items() if v is not None}
-
+        data = serializer.data
+        data["votes"] = {k: v for k, v in data.get("votes", {}).items() if v is not None}
         return data
 
     def save_vote(self, user_name, vote):
         meeting = self.get_meeting()
-        room = self.get_room()
+        room = get_object_or_404(Room, id=self.room_id)
 
         if not any(user_name in d for d in room.users):
             return {"error": "Participant doesn't exist"}
-        else:
-            meeting.votes[user_name] = vote
-            meeting.save()
-            return self.get_data()
+
+        meeting.votes[user_name] = vote
+        meeting.save()
+        return self.get_data()
