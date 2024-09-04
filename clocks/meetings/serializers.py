e6f274b2d8f17ed6ff8ec7649a170e71edcb17cf
phonik <askar.a.shakirov@gmail.com>
2024-09-05 00:18:56 +0300

    Убрал ненужный код

--- clocks/meetings/serializers.py
+++ clocks/meetings/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
-from rooms.models import Room
 
 from .models import Meeting
 
 
 class MeetingSerializer(serializers.ModelSerializer):
-    room_id = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
-    task_name = serializers.CharField(max_length=100)
-    votes = serializers.JSONField()
-    average_score = serializers.FloatField(allow_null=True, required=False)
-    active = serializers.BooleanField()
-
     class Meta:
         model = Meeting
         fields = ["id", "room", "task_name", "votes", "average_score", "active"]
