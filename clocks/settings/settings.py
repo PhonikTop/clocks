b34187b00c28dc9147e3968df3ecd3c72949a6ea
PhonikTop <askar.a.shakirov@gmail.com>
2024-09-06 23:51:55 +0300

    Починил статику для url

--- clocks/settings/settings.py
+++ clocks/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4uesufsmtlw(@ffq4ro%!he-2u=0-gpf$vfbg=tof+6_+4cr@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 # Application definition
 
@@ -61,7 +61,7 @@ ROOT_URLCONF = "settings.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates/")],
+        "DIRS": [os.path.join(BASE_DIR.parent, "templates/")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -128,11 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR.parent, "static/"),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
