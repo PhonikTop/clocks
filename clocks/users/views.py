ee24627ce03e27173c57c05a64cffed7229ddbdb
PhonikTop <askar.a.shakirov@gmail.com>
2024-09-26 19:45:39 +0300

    Переписал JoinRoomView под GenericAPIView и сделал создание куки для новых клиентов

--- clocks/users/views.py
+++ clocks/users/views.py
@@ -1,8 +1,12 @@
+import uuid
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from meetings.models import Meeting
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.response import Response
 from rooms.models import Room
 
 from .redis_client import RedisClient
@@ -11,16 +15,24 @@ from .serializers import UserInputSerializer
 redis = RedisClient()
 
 
-class JoinRoomView(CreateAPIView):
+class JoinRoomView(GenericAPIView):
     """
     Присоединение пользователя к комнате.
     """
     serializer_class = UserInputSerializer
 
-    def perform_create(self, serializer):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         nickname, role = serializer.validated_data["nickname"], serializer.validated_data["role"]
         cookie = self.request.COOKIES.get("user")
 
+        response = Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if cookie is None:
+            cookie = str(uuid.uuid4())
+            response.set_cookie("user", value=cookie, max_age=432000)
         if redis.check_token_in_db(cookie):
             raise ValidationError({"error": "User exists"})
 
@@ -34,6 +46,8 @@ class JoinRoomView(CreateAPIView):
 
         room.current_meeting = meeting
         room.save()
+
+        return response
 
 
 class CurrentUserView(RetrieveAPIView):
