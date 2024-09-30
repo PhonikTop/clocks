6c7d040e34a5c9b647f3f47cf80f4ab755429285
PhonikTop <askar.a.shakirov@gmail.com>
2024-09-30 22:01:20 +0300

    Добавил в settings.py объявление папок

--- clocks/settings/settings.py
+++ clocks/settings/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -26,6 +26,10 @@ SECRET_KEY = "django-insecure-4uesufsmtlw(@ffq4ro%!he-2u=0-gpf$vfbg=tof+6_+4cr@*
 DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+
+sys.path.insert(1, str(BASE_DIR))
+for app_lookup_path in ("clocks",):
+    sys.path.insert(1, os.path.normpath(os.path.join(BASE_DIR, app_lookup_path)))
 
 # Application definition
 
