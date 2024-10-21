fec31a4b2df2339b5bf86fa8582ae52d5158b869
PhonikTop <askar.a.shakirov@gmail.com>
2024-10-22 01:05:16 +0300

    Исправил ошибку

--- clocks/users/redis_client.py
+++ clocks/users/redis_client.py
@@ -1,21 +1,21 @@
 from django.core.cache import cache
 
 
-def save_new_client_to_cache(self, token, nickname: str, role: str, ttl: int = 432000) -> None:
+def save_new_client_to_cache(token, nickname: str, role: str, ttl: int = 432000) -> None:
     """
     Сохранение данных клиента в кэш с использованием token.
     """
     cache.set(token, {"nickname": nickname, "role": role}, ttl)
 
 
-def check_token_in_cache(self, token: str) -> bool:
+def check_token_in_cache(token: str) -> bool:
     """
     Проверка на существование записи в кэше.
     """
     return cache.get(token) is not None
 
 
-def get_client_data_by_token(self, token: str) -> dict[str, str]:
+def get_client_data_by_token(token: str) -> dict[str, str]:
     """
     Получение данных клиента (никнейм и роль) по токену из кэша.
     """
