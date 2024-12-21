1226dad7d28eb4136f54c6cf575d8cf340ad3fef
PhonikTop <askar.a.shakirov@gmail.com>
2024-12-21 20:31:38 +0300

    Улучшение логики и оптимизация кода

--- clocks/ws/actions.py
+++ clocks/ws/actions.py
@@ -8,36 +8,39 @@ from .handlers import ActionHandler
 
 
 class SubmitVoteAction(BaseAction):
-
     async def get_queryset(self):
-        return Meeting.objects.filter(room=self.consumer.lookup_id, active=True)
+        return Meeting.objects.filter(room=self.consumer.lookup_id, active=True).select_related()
 
     async def perform_action(self):
-        user_name = await self.get_param("user_id")
+        user_id = await self.get_param("user_id")
         try:
             vote = int(await self.get_param("vote"))
         except (ValueError, TypeError):
-            return {"error": "Invalid vote"}
+            return {"error": "Invalid vote format"}
 
         meeting = await self.get_object()
         room_cache = RoomCacheManager(self.consumer.lookup_id)
 
-        participants = await sync_to_async(room_cache.get_users_by_role)("voter")
-        votes = await sync_to_async(room_cache.get_votes)()
+        participants, votes = await sync_to_async(
+            lambda: (room_cache.get_users_by_role("voter"), room_cache.get_votes())
+        )()
 
-        if user_name not in participants:
+        if user_id not in participants:
             return {"error": "Participant not found"}
-        if user_name in votes:
+        if user_id in votes:
             return {"error": "Participant already voted"}
 
-        await sync_to_async(room_cache.set_vote)(user_name, vote)
+        await sync_to_async(room_cache.set_vote)(user_id, vote)
 
         if len(participants) == len(votes) + 1:
             votes = await sync_to_async(room_cache.get_votes)()
             await sync_to_async(meeting_results)(meeting)
-            return {"votes": votes, "average_score": meeting.average_score}
+            return {
+                "votes": votes,
+                "average_score": meeting.average_score,
+            }
 
-        return {"status": "Vote recorded", "user": "user_name"}
+        return {"status": "Vote recorded", "user": user_id}
 
 
 action_handler = ActionHandler()
