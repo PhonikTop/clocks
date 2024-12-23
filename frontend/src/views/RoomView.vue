4eb06e44caf4219b10992995d93bc5e59c1b0174
PhonikTop <askar.a.shakirov@gmail.com>
2024-12-24 02:17:07 +0300

    Добавил в RoomView остальные компоненты

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -2,16 +2,31 @@
   <div class="content">
     <div class="wrapper">
       <StartBlock/>
+      <GuessBlock/>
+      <TotalBlock/>
+      <WaitBlock/>
+      <ObserversWrapper/>
+      <UserWrapper/>
     </div>
   </div>
 </template>
 <script>
 import StartBlock from '../components/room/StartBlock.vue'
+import GuessBlock from '@/components/room/GuessBlock.vue'
+import TotalBlock from '@/components/room/TotalBlock.vue'
+import WaitBlock from '@/components/room/WaitBlock.vue'
+import ObserversWrapper from '@/components/room/ObserversWrapper.vue'
+import UserWrapper from '@/components/room/UserWrapper.vue'
 
 export default {
   name: 'RoomView',
   components: {
-    StartBlock
+    StartBlock,
+    GuessBlock,
+    TotalBlock,
+    WaitBlock,
+    ObserversWrapper,
+    UserWrapper
   }
 }
 </script>
