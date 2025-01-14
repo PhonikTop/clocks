eb27f1708eb097140ede0e58c2d4e9369a6ef724
PhonikTop <askar.a.shakirov@gmail.com>
2025-01-14 17:34:14 +0300

    feat: Add user info endpoint

--- clocks/users/urls.py
+++ clocks/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import JoinRoomView
+from .views import JoinRoomView, UserInfoView
 
 urlpatterns = [
     path("join/<int:pk>/", JoinRoomView.as_view(), name="join_room"),
+    path("<int:pk>/", UserInfoView.as_view(), name="user_info"),
 ]
