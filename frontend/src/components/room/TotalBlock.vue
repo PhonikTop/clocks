b8766b8d50e5c25e87608b9c588577915a164963
PhonikTop <askar.a.shakirov@gmail.com>
2025-01-18 15:48:32 +0300

    feat: add meeting action buttons (restart, next, finish) in total block

--- frontend/src/components/room/TotalBlock.vue
+++ frontend/src/components/room/TotalBlock.vue
@@ -2,12 +2,16 @@
     <div class="total__block">
         <label>Общая оценка:</label>
         <div>
-        <span></span> ч.
+          <span></span> ч.
         </div>
-        <button id="again">Еще раз</button>
+        <div class="choise_buttons">
+          <button class="choise_button">Перезапустить</button>
+          <button class="choise_button">Далее</button>
+          <button class="choise_button">Завершить</button>
+        </div>
     </div>
 </template>
-<style>
+<style scoped>
 .total__block label {
   font-size: 1rem;
   margin-bottom: 5px;
@@ -20,9 +24,18 @@
   color: #6B6B6B;
 }
 
-#again {
-  margin-top: 10px;
-  font-size: 1rem;
+.choise_buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  gap: 12px;
+  margin-top: 14px;
+}
+
+.choise_button {
+  font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 17px;
   color: #fff;
   background: #7F67F1;
   padding: 5px 10px;
