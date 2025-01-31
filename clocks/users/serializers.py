3d3e64e7035a96b39a319c8c2f197a2d9d1dc645
PhonikTop <askar.a.shakirov@gmail.com>
2025-01-31 23:47:33 +0300

    refactor(UserInputSerializer): Use TextChoices for roles

--- clocks/users/serializers.py
+++ clocks/users/serializers.py
@@ -1,11 +1,15 @@
 from meetings.models import Meeting
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from django.db import models
 
+class UserRoleChoices(models.TextChoices):
+    OBSERVER = 'observer', 'Observer'
+    VOTER = 'voter', 'Voter'
 
 class UserInputSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=25)
-    role = serializers.ChoiceField(choices=[("observer", "observer"), ("voter", "voter")])
+    role = serializers.ChoiceField(choices=UserRoleChoices.choices)
 
     def validate(self, data):
         room = self.context.get("room")
