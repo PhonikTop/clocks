6b499b80e954e91a6a8204f5a8e67e48fdacbc6f
AskSH <askar.a.shakirov@gmail.com>
2025-04-01 15:32:52 +0300

    chore(routing): disable deprecated WebSocket test page

--- clocks/settings/urls.py
+++ clocks/settings/urls.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from django.urls import include, path
 
 
-def Test_Page(request, room_id):
-    return render(request, "index.html", {"room_id": room_id})
+# def Test_Page(request, room_id):
+#     return render(request, "index.html", {"room_id": room_id})
 
 
 urlpatterns = [
     path("api/v1/", include("api.urls")),
-    path("room/<int:room_id>/", Test_Page),
+    # path("room/<int:room_id>/", Test_Page),
     path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
 ]
