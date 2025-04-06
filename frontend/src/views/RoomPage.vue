81240607fb44b4f205cd2340c5ea118403840bc3
AskSH <askar.a.shakirov@gmail.com>
2025-04-06 16:49:28 +0300

    feat(room): add room page basics

---
+++ frontend/src/views/RoomPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+
+const roomId = computed(() => route.params.room_id);
+
+const leaveRoom = () => {
+  router.push({ name: "Login" });
+};
+</script>
+
+<template>
+  <h1>Комната {{ roomId }}</h1>
+  <div class="room">
+    <button @click="leaveRoom">Выйти из комнаты</button>
+  </div>
+</template>
