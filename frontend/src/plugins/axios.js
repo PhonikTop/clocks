e8dfb2b7756710f07bf4c16894103e2e95160050
AskSH <askar.a.shakirov@gmail.com>
2025-04-14 04:10:08 +0300

    fix: update API baseURL to valid value

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:80',
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:80/api/v1/",
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
