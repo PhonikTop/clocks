0bc5a76ae83e0cb248231b10d87ee93bfc9e359e
AskSH <askar.a.shakirov@gmail.com>
2025-04-14 04:02:52 +0300

    feat: implement room list fetching via useRoom

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,18 +1,14 @@
 <script setup>
+import { onMounted } from "vue";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import useRoom from "@/composables/useRoom";
 
 const router = useRouter();
 const username = ref("");
 const roomId = ref("");
 
-const rooms = ref([
-  { id: "1", name: "Room 1" },
-  { id: "2", name: "Room 2" },
-  { id: "3", name: "Room 3" },
-  { id: "4", name: "Room 4" },
-  { id: "5", name: "Room 5" },
-]);
+const { roomList, fetchRoomList } = useRoom();
 
 const enterRoom = () => {
   router.push({
@@ -20,6 +16,10 @@ const enterRoom = () => {
     params: { room_id: roomId.value },
   });
 };
+
+onMounted(async () => {
+  await fetchRoomList();
+});
 </script>
 
 <template>
@@ -34,7 +34,7 @@ const enterRoom = () => {
         <label>ID комнаты:</label>
         <select v-model="roomId" required>
           <option disabled value="">Выберите комнату</option>
-          <option v-for="room in rooms" :key="room.id" :value="room.id">
+          <option v-for="room in roomList" :key="room.id" :value="room.id">
             {{ room.name }}
           </option>
         </select>
