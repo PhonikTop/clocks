5cff5868e8d00f7f78da2142e6c40d4fee175524
AskSH <askar.a.shakirov@gmail.com>
2025-04-18 02:52:05 +0300

    fix(serializer): remove redundant validate method

--- clocks/users/serializers.py
+++ clocks/users/serializers.py
@@ -10,11 +10,3 @@ class UserRoleChoices(models.TextChoices):
 class UserInputSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=25)
     role = serializers.ChoiceField(choices=UserRoleChoices.choices)
-
-    def validate(self, data):
-        room = self.context.get("room")
-
-        if not Meeting.objects.filter(room_id=room.id, active=True).exists():
-            raise ValidationError({"error": "No active meeting in the room"})
-
-        return data
