6d357e4c65689b324a44b95c960584fafdaf1252
AskSH <askar.a.shakirov@gmail.com>
2025-04-18 03:07:39 +0300

    chore: apply code style corrections

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,23 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL || "http://localhost:80/api/v1/",
   timeout: 20000,
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-})
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 api.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token')
+  (config) => {
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
-  error => Promise.reject(error)
-)
+  (error) => Promise.reject(error)
+);
 
-export default api
+export default api;
