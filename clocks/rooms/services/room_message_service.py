f43585041b121d0dc07e725e09cf4ccea3baceb1
AskSH <askar.a.shakirov@gmail.com>
2025-04-20 12:30:53 +0300

    refactor(websocket): restructure messages with adding user uuid

--- clocks/rooms/services/room_message_service.py
+++ clocks/rooms/services/room_message_service.py
@@ -5,7 +5,7 @@ class RoomMessageService:
     def __init__(self, message_sender: MessageSender):
         self.message_sender = message_sender
 
-    def notify_user_joined(self, room_id: int, nickname: str, role: str) -> None:
+    def notify_user_joined(self, room_id: int, uuid: str, nickname: str, role: str) -> None:
         """
         Уведомляет о присоединении пользователя к комнате.
 
@@ -17,7 +17,10 @@ class RoomMessageService:
         group_name = f"room_{room_id}"
         message = {
             "type": "user_joined",
-            "user": nickname,
-            "role": role,
+            "user": {uuid: {
+                "nickname": nickname,
+                "role": role,
+                "vote": None
+            }}
         }
         self.message_sender.send(group_name, message)
