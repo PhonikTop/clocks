bcb095c8f473ae06156b03e0631c887cf23488e6
AskSH <askar.a.shakirov@gmail.com>
2025-06-19 16:53:43 +0300

    feat: extend room message service with cache integration and new events

--- clocks/rooms/services/room_message_service.py
+++ clocks/rooms/services/room_message_service.py
@@ -1,26 +1,59 @@
+from typing import Optional
+
 from .message_senders.base import MessageSender
+from .room_cache_service import RoomCacheService
 
 
 class RoomMessageService:
-    def __init__(self, message_sender: MessageSender):
+    def __init__(self, room_id: int, message_sender: MessageSender, room_cache_service: Optional[RoomCacheService] = None):
+        self.room_id = room_id
         self.message_sender = message_sender
+        self._room_cache_service = room_cache_service
 
-    def notify_user_joined(self, room_id: int, uuid: str, nickname: str, role: str) -> None:
+    @property
+    def room_cache_service(self):
+        if self._room_cache_service is None:
+            raise ValueError("room_cache_service is missing")
+        return self._room_cache_service
+
+    @property
+    def _group_name(self):
+        return f"room_{self.room_id}"
+
+    def notify_user_joined(self, uuid: str) -> str | None:
         """
         Уведомляет о присоединении пользователя к комнате.
 
         Args:
-            room_id (int): Идентификатор комнаты.
-            nickname (str): Никнейм пользователя.
-            role (str): Роль пользователя.
+            uuid (str) : Идентификатор пользователя.
         """
-        group_name = f"room_{room_id}"
+        user_data = self.room_cache_service.get_user(uuid)
+
         message = {
             "type": "user_joined",
-            "user": {uuid: {
-                "nickname": nickname,
-                "role": role,
-                "vote": None
-            }}
+            "user": {uuid: user_data}
         }
-        self.message_sender.send(group_name, message)
+        self.message_sender.send(self._group_name, message)
+
+    def send_room_voted_users(self):
+        votes = self.room_cache_service.get_votes()
+
+        message = {
+            "type": "voted_users_update",
+            "voted_users": list(votes.keys())
+        }
+        self.message_sender.send(self._group_name, message)
+
+    def notify_meeting_task_name_changed(self, new_task_name:str):
+        message = {
+            "type": "task_name_changed",
+            "new_task_name": new_task_name
+        }
+        self.message_sender.send(self._group_name, message)
+
+    def notify_meeting_started(self, meeting_id: int):
+        message = {
+            "type": "meeting_started",
+            "id": meeting_id,
+        }
+        self.message_sender.send(self._group_name, message)
