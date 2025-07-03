f44d4c3bb6091e54a52a301806f47c4cec6433cf
AskSH <askar.a.shakirov@gmail.com>
2025-07-03 22:46:56 +0300

    chore: удален устаревший TestPage

--- clocks/settings/urls.py
+++ clocks/settings/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from django.shortcuts import render
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-# def Test_Page(request, room_id):
-#     return render(request, "index.html", {"room_id": room_id})
 
 
 urlpatterns = [
     path("api/v1/", include("api.urls")),
-    # path("room/<int:room_id>/", Test_Page),
     path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
