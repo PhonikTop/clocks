9f5a69f95d1911ec1dfcc59a1d3eb77e7303bc5d
AskSH <askar.a.shakirov@gmail.com>
2025-07-03 21:20:08 +0300

    feat: добавлена поддержка drf-spectacular для генерации схем OpenAPI

--- clocks/settings/urls.py
+++ clocks/settings/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import include, path
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # def Test_Page(request, room_id):
 #     return render(request, "index.html", {"room_id": room_id})
@@ -12,4 +12,6 @@ urlpatterns = [
     # path("room/<int:room_id>/", Test_Page),
     path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
