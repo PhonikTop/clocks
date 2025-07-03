64161b218dca2b22cc10f90b55f75c1ae0435dfa
AskSH <askar.a.shakirov@gmail.com>
2025-07-03 21:21:55 +0300

    docs: добавлена документация для Rooms

--- clocks/rooms/urls.py
+++ clocks/rooms/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path("", RoomCreateView.as_view(), name="room_create"),
     path("list/", RoomListView.as_view(), name="room_list"),
-    path("<int:pk>/", RoomDetailView.as_view(), name="room_detail"),
-    path("<int:pk>/participants/", RoomParticipantsView.as_view(), name="room_participants"),
     path("<int:pk>/history/", RoomHistoryView.as_view(), name="room_history"),
+    path("<int:id>/", RoomDetailView.as_view(), name="room_detail"),
+    path("<int:id>/participants/", RoomParticipantsView.as_view(), name="room_participants"),
 ]
