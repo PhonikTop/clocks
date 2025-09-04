1fa7433b3026be5e8e13e29619b0e7a0bf7a64cb
AskSH <askar.a.shakirov@gmail.com>
2025-09-05 02:26:18 +0300

    chore(frontend): обновлен конфиг esling

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,12 +2,60 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
 import json from "@eslint/json";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  {ignores: ["node_modules/**", "dist/**", ".vscode/**"],},
+  js.configs.recommended,
+
+  // Vue
+  ...pluginVue.configs["flat/recommended"],
+
+  // TypeScript
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        extraFileExtensions: [".vue"],
+      },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      ...ts.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      // ...ts.configs["recommended-requiring-type-checking"].rules,
+    },
+  },
+
+  // Vue + TS в <script>
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+        project: "./tsconfig.json",
+        extraFileExtensions: [".vue"],
+      },
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      ...ts.configs.recommended.rules,
+      "vue/no-undef-properties": "error",
+    },
+  },
+
+  // JSON
+  {
+    files: ["**/*.json"],
+    plugins: { json: json },
+    rules: {
+      ...json.configs.recommended.rules,
+    },
+  },
+
+  { ignores: ["node_modules/**", "dist/**", ".vscode/**"] },
 ]);
