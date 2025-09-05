30d23f2933bfdb7e4739be04e0164c3e31a16055
AskSH <askar.a.shakirov@gmail.com>
2025-09-05 14:26:54 +0300

    fix(frontend): обновлен импорт

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { provide } from 'vue';
-import { useToasts } from '@/composables/useToasts.js';
+import { useToasts } from '@/composables/useToasts.ts';
 import ToastManager from "@/components/ToastManager.vue";
 
 const toast = useToasts()
