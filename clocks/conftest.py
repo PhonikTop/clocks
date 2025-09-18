824fb479cf3658062d564e895d925a14d8c8af9e
AskSH <askar.a.shakirov@gmail.com>
2025-09-19 00:24:56 +0300

    feat(backend): новые фикстуры для тестов

--- clocks/conftest.py
+++ clocks/conftest.py
@@ -8,3 +8,26 @@ from rest_framework.test import APIClient
 @pytest.fixture
 def api_client():
     return APIClient()
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(username="user1", password="pass")
+
+@pytest.fixture
+def room(db):
+    from rooms.models import Room
+
+    return Room.objects.create(name="Test Room")
+
+
+@pytest.fixture
+def meeting(db, room):
+    from meetings.models import Meeting
+
+    return Meeting.objects.create(room=room, task_name="Initial Task")
+
+@pytest.fixture
+def finished_meeting(db, room):
+    from meetings.models import Meeting
+
+    return Meeting.objects.create(room=room, task_name="Initial Task", average_score=4)
