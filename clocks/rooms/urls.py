cff86f90baaaa4579bd6f7293df87d3bab679e66
AskSH <askar.a.shakirov@gmail.com>
2025-09-24 21:44:33 +0300

    refactor(backend): обновлены URL API таймера комнаты

--- clocks/rooms/urls.py
+++ clocks/rooms/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("list/", RoomListView.as_view(), name="room_list"),
     path("<int:pk>/", RoomDetailView.as_view(), name="room_detail"),
     path("<int:pk>/participants/", RoomParticipantsView.as_view(), name="room_participants"),
-    path("<int:pk>/timer_start", SetRoomTimer.as_view(), name="set_room_timer"),
-    path("<int:pk>/get_timer_start", GetRoomTimer.as_view(), name="get_room_timer"),
-    path("<int:pk>/reset_timer_start", ResetRoomTimer.as_view(), name="reset_room_timer"),
+    path("<int:pk>/timer_start/", SetRoomTimer.as_view(), name="set_room_timer"),
+    path("<int:pk>/get_timer/", GetRoomTimer.as_view(), name="get_room_timer"),
+    path("<int:pk>/reset_timer/", ResetRoomTimer.as_view(), name="reset_room_timer"),
 ]
