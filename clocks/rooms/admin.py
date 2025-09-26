545be0cf5c75c861aa24c8fc8ccdeeb4517eb746
AskSH <askar.a.shakirov@gmail.com>
2025-09-26 13:07:44 +0300

    refactor(backend): страница с историей голосований переделана под Voting

--- clocks/rooms/admin.py
+++ clocks/rooms/admin.py
@@ -14,21 +14,21 @@ class RoomAdmin(admin.ModelAdmin):
 
     # Добавление ссылки на историю голосований
     def view_history(self, obj):
-        return mark_safe(f'<a href="/admin/rooms/room/{obj.id}/meeting-history/">История голосований</a>')
+        return mark_safe(f'<a href="/admin/rooms/room/{obj.id}/voting-history/">История голосований</a>')
 
     view_history.allow_tags = True
     view_history.short_description = "История голосований"
 
 
-def meeting_history_view(request, room_id):
+def voting_history_view(request, room_id):
     room = get_object_or_404(Room, id=room_id)
-    meetings = Voting.objects.filter(room=room).order_by("-created")
+    votings = Voting.objects.filter(room=room).order_by("-created")
 
-    paginator = Paginator(meetings, 10)  # 10 записей на страницу
+    paginator = Paginator(votings, 10)  # 10 записей на страницу
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, "admin/meeting_history.html", {
+    return render(request, "admin/voting_history.html", {
         "room": room,
         "page_obj": page_obj,
     })
@@ -37,7 +37,7 @@ def meeting_history_view(request, room_id):
 def get_urls(self):
     urls = super(RoomAdmin, self).get_urls()
     custom_urls = [
-        path("<int:room_id>/meeting-history/", self.admin_site.admin_view(meeting_history_view))
+        path("<int:room_id>/voting-history/", self.admin_site.admin_view(voting_history_view))
     ]
     return custom_urls + urls
 
