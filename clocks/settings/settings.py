4e01626a38c248b5aba900b1ff98f3e35127084c
AskSH <askar.a.shakirov@gmail.com>
2025-09-29 21:38:30 +0300

    feat(backend): установлен и настроен structlog для logging

--- clocks/settings/settings.py
+++ clocks/settings/settings.py
@@ -1,4 +1,17 @@
 import os
+import sys
+import logging
+
+import structlog
+from structlog import contextvars
+from structlog.stdlib import ProcessorFormatter, add_logger_name
+from structlog.processors import (
+    TimeStamper,
+    add_log_level,
+    format_exc_info,
+    JSONRenderer,
+)
+from structlog.dev import ConsoleRenderer
 from pathlib import Path
 
 import dj_database_url
@@ -11,6 +24,69 @@ PROJECT_ROOT = BASE_DIR.parent
 
 SECRET_KEY = get_env_param_str("SECRET_KEY", "dev")
 DEBUG = get_env_param_bool("DEBUG", False)
+
+foreign_pre_chain = [
+    structlog.contextvars.merge_contextvars,
+    add_log_level,
+    TimeStamper(fmt="iso", utc=True),
+    format_exc_info,
+]
+
+if not DEBUG:
+    renderer = JSONRenderer()
+    root_level = "INFO"
+else:
+    renderer = ConsoleRenderer(colors=True)
+    root_level = "DEBUG"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "struct": {
+            "()": ProcessorFormatter,
+            "processor": renderer,
+            "foreign_pre_chain": foreign_pre_chain,
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": root_level,
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "struct",
+        },
+    },
+    "root": {
+        "handlers": ["default"],
+        "level": root_level,
+    },
+    "loggers": {
+        "django": {"handlers": ["default"], "level": root_level, "propagate": False},
+        "django.db.backends": {"handlers": ["default"], "level": "WARNING", "propagate": False},
+        "django.template": {"handlers": ["default"], "level": "WARNING", "propagate": False},
+        "asgiref": {"handlers": ["default"], "level": "WARNING", "propagate": False},
+        "django.request": {"handlers": ["default"], "level": "ERROR", "propagate": False},
+        "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "uvicorn.error": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "uvicorn.access": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "channels": {"handlers": ["default"], "level": "INFO", "propagate": False},
+    },
+}
+
+structlog.configure(
+    processors=[
+        add_logger_name,
+        contextvars.merge_contextvars,
+        add_log_level,
+        TimeStamper(fmt="iso"),
+        format_exc_info,
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    cache_logger_on_first_use=True,
+)
 
 ALLOWED_HOSTS = get_env_param_list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 CSRF_TRUSTED_ORIGINS = get_env_param_list("CSRF_TRUSTED_ORIGINS", default=[])
