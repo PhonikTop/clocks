2332cbc7b409e60ec330b1fdaa3a30f4595bda83
AskSH <askar.a.shakirov@gmail.com>
2025-10-05 14:41:06 +0300

    fix(frontend): исправлен путь до API и WS

--- frontend/src/views/RoomPage.vue
+++ frontend/src/views/RoomPage.vue
@@ -53,7 +53,7 @@ const redirectToLogin = () => router.push({ name: "Login" });
 
 const { isConnected, connect, sendMessage, addMessageHandler } =
   useRoomWebSocket(
-    `${import.meta.env.VITE_WS_BASE_URL}room/${roomId.value}/?token=${token.value}`
+    `/${roomId.value}/?token=${token.value}`
   );
 
 const { currentVoting } = useRoomWebSocketHandler(
